6fb9e9d9d7d8d5659a05244e795812a18e700430
mmooij <michel.mooij7@gmail.com>
2024-07-05 23:55:21 +0200

    Use gson for (de)serialization of QRcode config data

--- app/src/main/java/com/example/nuki_sesami_app/base/QRConfig.kt
+++ app/src/main/java/com/example/nuki_sesami_app/base/QRConfig.kt
@@ -2,6 +2,7 @@ package com.example.nuki_sesami_app.base
 
 import android.graphics.Bitmap
 import com.example.nuki_sesami_app.R
+import com.google.gson.Gson
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
@@ -10,65 +11,35 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 class QRConfig(
     preferences: UserPreferences
 ) {
-    private val items: Map<String, Any> = getItems(preferences)
-
-    private fun getItems(preferences: UserPreferences): Map<String, Any> {
-        val nukiDeviceID = preferences.load(
+    private val data = QRConfigData(
+        nukiDeviceID = preferences.load(
             R.string.preferences_key_nuki_device_id, NUKI_SESAMI_DEFAULT_DEVICE_ID
-        )
-        val mqttHostname = preferences.load(
+        ),
+        mqttHostname = preferences.load(
             R.string.preferences_key_mqtt_hostname, NUKI_SESAMI_DEFAULT_MQTT_HOSTNAME
-        )
-        val mqttPort = preferences.load(
+        ),
+        mqttPort = preferences.load(
             R.string.preferences_key_mqtt_port, NUKI_SESAMI_DEFAULT_MQTT_PORT
-        )
-        val mqttUsername = preferences.load(
+        ),
+        mqttUsername = preferences.load(
             R.string.preferences_key_mqtt_username, NUKI_SESAMI_DEFAULT_MQTT_USERNAME
-        )
-        val mqttPassword = preferences.load(
+        ),
+        mqttPassword = preferences.load(
             R.string.preferences_key_mqtt_password, NUKI_SESAMI_DEFAULT_MQTT_PASSWORD
-        )
-        val bluetoothAddress = preferences.load(
+        ),
+        bluetoothAddress = preferences.load(
             R.string.preferences_key_bluetooth_address, NUKI_SESAMI_DEFAULT_BLUETOOTH_ADDRESS
-        )
-        val bluetoothChannel = preferences.load(
+        ),
+        bluetoothChannel = preferences.load(
             R.string.preferences_key_bluetooth_channel, NUKI_SESAMI_DEFAULT_BLUETOOTH_CHANNEL
-        )
-        val preferBluetooth = preferences.load(
+        ),
+        preferBluetooth = preferences.load(
             R.string.preferences_key_prefer_bluetooth, false
         )
-
-        return mapOf(
-            "nukiDeviceID" to nukiDeviceID,
-            "mqttHostname" to mqttHostname,
-            "mqttPort" to mqttPort,
-            "mqttUsername" to mqttUsername,
-            "mqttPassword" to mqttPassword,
-            "bluetoothAddress" to bluetoothAddress,
-            "bluetoothChannel" to bluetoothChannel,
-            "preferBluetooth" to preferBluetooth,
-        )
-    }
-
-    fun toJson(): String {
-        val json = StringBuilder()
-        json.append("{")
-        items.entries.forEach { entry ->
-            val value = when (entry.value) {
-                is String -> "\"${entry.value}\""
-                else -> entry.value.toString()
-            }
-            json.append("\"${entry.key}\": $value,")
-        }
-        if (json.endsWith(",")) {
-            json.deleteCharAt(json.length - 1)
-        }
-        json.append("}")
-        return json.toString()
-    }
+    )
 
     fun generateQRCode(width: Int, height: Int): Bitmap {
-        val text = this.toJson()
+        val text = Gson().toJson(data)
         val hints = mapOf(EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.H)
         val bitMatrix = QRCodeWriter().encode(text, BarcodeFormat.QR_CODE, width, height, hints)
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
